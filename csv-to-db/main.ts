a52b4f441c9244a3ed91557c575752eec5190083
VeronikaBogdanovich <veronicka.angel2012@yandex.by>
2018-11-05 18:19:13 +0300

    csv parse

--- csv-to-db/main.ts
+++ csv-to-db/main.ts
@@ -11,127 +11,127 @@ import {createConnection} from 'mysql';
  */
 
 const SEPARATOR: string = ',';
+export const NEED_SAVE_DB: boolean = false;
 
 export function validate(data: CsvModel): boolean {
-	if(!data) {
-		return false;
-	}
+    if (!data) {
+        return false;
+    }
 
-	const errors: any = {};
+    const errors: any = {};
 
-	// name
-	// @ts-ignore
-	let validationObj = config.csv.find(obj => obj.name === 'name');
-	errors.name = checkLength(validationObj.minLength, validationObj.maxLength)(data.name)
-		&& new RegExp(validationObj.regExp, 'g').test(data.name);
+    // name
+    // @ts-ignore
+    let validationObj = config.csv.find(obj => obj.name === 'name');
+    errors.name = checkLength(validationObj.minLength, validationObj.maxLength)(data.name)
+        && new RegExp(validationObj.regExp, 'g').test(data.name);
 
-	// age
-	// @ts-ignore
-	validationObj = config.csv.find(obj => obj.name === 'age');
-	errors.age = checkLength(validationObj.minLength, validationObj.maxLength)(data.age)
-		&& new RegExp(validationObj.regExp, 'g').test(data.age)
-		&& checkMinMax(validationObj.min, validationObj.max)(+data.age);
+    // age
+    // @ts-ignore
+    validationObj = config.csv.find(obj => obj.name === 'age');
+    errors.age = checkLength(validationObj.minLength, validationObj.maxLength)(data.age)
+        && new RegExp(validationObj.regExp, 'g').test(data.age)
+        && checkMinMax(validationObj.min, validationObj.max)(+data.age);
 
-	// date
-	// @ts-ignore
-	validationObj = config.csv.find(obj => obj.name === 'date');
-	const date = new RegistrationDate(data.date);
-	errors.date = checkLength(validationObj.minLength, validationObj.maxLength)(date)
-		&& checkMinMax(validationObj.minYear, validationObj.maxYear)(date.year)
-		&& checkMinMax(validationObj.minMonth, validationObj.maxMonth)(date.month)
-		&& checkMinMax(validationObj.minDay, validationObj.maxDay)(date.day);
+    // date
+    // @ts-ignore
+    validationObj = config.csv.find(obj => obj.name === 'date');
+    const date = new RegistrationDate(data.date);
+    errors.date = checkLength(validationObj.minLength, validationObj.maxLength)(date)
+        && checkMinMax(validationObj.minYear, validationObj.maxYear)(date.year)
+        && checkMinMax(validationObj.minMonth, validationObj.maxMonth)(date.month)
+        && checkMinMax(validationObj.minDay, validationObj.maxDay)(date.day);
 
-	// time
-	// @ts-ignore
-	validationObj = config.csv.find(obj => obj.name === 'time');
-	const time = new RegistrationTime(data.time);
-	errors.time = checkLength(validationObj.minLength, validationObj.maxLength)(time)
-		&& checkMinMax(validationObj.minHour, validationObj.maxHour)(time.hours)
-		&& checkMinMax(validationObj.minMinute, validationObj.maxMinute)(time.minutes);
+    // time
+    // @ts-ignore
+    validationObj = config.csv.find(obj => obj.name === 'time');
+    const time = new RegistrationTime(data.time);
+    errors.time = checkLength(validationObj.minLength, validationObj.maxLength)(time)
+        && checkMinMax(validationObj.minHour, validationObj.maxHour)(time.hours)
+        && checkMinMax(validationObj.minMinute, validationObj.maxMinute)(time.minutes);
 
-	// email
-	// @ts-ignore
-	validationObj = config.csv.find(obj => obj.name === 'email');
-	const email = new Email(data.email);
-	errors.email = checkLength(validationObj.minLength, validationObj.maxLength)(email)
-		&& new RegExp(validationObj.regExpName, 'g').test(email.name)
-		&& new RegExp(validationObj.regExpDomain).test(email.domain);
+    // email
+    // @ts-ignore
+    validationObj = config.csv.find(obj => obj.name === 'email');
+    const email = new Email(data.email);
+    errors.email = checkLength(validationObj.minLength, validationObj.maxLength)(email)
+        && new RegExp(validationObj.regExpName, 'g').test(email.name)
+        && new RegExp(validationObj.regExpDomain).test(email.domain);
 
-	// phone
-	// @ts-ignore
-	validationObj = config.csv.find(obj => obj.name === 'phone');
-	const phone = new Phone(data.phone);
-	errors.phone = checkLength(validationObj.minLength, validationObj.maxLength)(phone);
+    // phone
+    // @ts-ignore
+    validationObj = config.csv.find(obj => obj.name === 'phone');
+    const phone = new Phone(data.phone);
+    errors.phone = checkLength(validationObj.minLength, validationObj.maxLength)(phone);
 
-	// return false is errors object has false in any property
-	// console.log('-->', errors);
-	return !Object.keys(errors).some(item => !errors[item])
+    // return false is errors object has false in any property
+    // console.log('-->', errors);
+    return !Object.keys(errors).some(item => !errors[item])
 }
 
 const results: Array<CsvModel> = [];
 const errors: Array<CsvModel> = [];
 
-function saveToDB(results: Array<CsvModel>): void {
-    if(!results.length) {
-    	return;
-	}
+export function saveToDB(validObjects: Array<CsvModel>): void {
+    if (!validObjects || !validObjects.length) {
+        return;
+    }
 
     const connection = createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'course_db'
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'course_db'
     });
 
     connection.connect((err) => {
-    	if (err) throw err;
-    	console.log('CONNECTED TO DB:');
+        if (err) throw err;
+        console.log('CONNECTED TO DB:');
 
 
-        results.forEach((item, index) => {
-            const sql = `INSERT INTO users (id, name, age, email, date, time, phone) ` +
-                `VALUES (${+item.id}, \'${item.name}\', \'${item.age}\', \'${item.email}\', \'${item.date}\', \'${item.time}\', \'${item.phone}\')`;
+        validObjects.forEach((item, index) => {
+            const sql = `INSERT INTO users (id, name, age, email, date, time, phone) VALUES (${+item.id}, \'${item.name}\', \'${item.age}\', \'${item.email}\', \'${item.date}\', \'${item.time}\', \'${item.phone}\')`;
 
-            connection.query(sql, (err, result)  => {
+            connection.query(sql, (err, result) => {
                 if (err) throw err;
                 console.log("1 record inserted");
 
-                if(index === result[result.length -1]) {
+                if (index === validObjects.length - 1) {
                     connection.end();
-				}
+                }
             });
 
 
         });
-	});
+    });
 }
 
 function makeCsvFile(array: Array<CsvModel>): void {
-	if (array.length) {
-		let writer = fs.createWriteStream('assets/invalid.csv');
+    if (array.length) {
+        let writer = fs.createWriteStream('assets/invalid.csv');
 
-		writer.write(Object.keys(array[0]).join(SEPARATOR) + '\n');
-		array.forEach(item => {
-			let arr = [];
+        writer.write(Object.keys(array[0]).join(SEPARATOR) + '\n');
+        array.forEach((item, index) => {
+            let arr = [];
 
-			for (let key in item) {
-				arr.push(item[key]);
-			}
+            for (let key in item) {
+                arr.push(item[key]);
+            }
 
-			writer.write(arr.join(SEPARATOR) + '\n');
-		});
-
-		writer.close();
-	}
+            writer.write(arr.join(SEPARATOR) + '\n');
+        });
+    }
 }
 
 fs.createReadStream('assets/Users.csv')
-	.pipe(csv({separator: SEPARATOR}))
-	.on('data', (data: CsvModel) => {
-		(validate(data) ? results : errors).push(data);
-	})
-	.on('end', () => {
-		saveToDB(results);
-		makeCsvFile(errors);
-		console.log('res', results);
-	});
+    .pipe(csv({separator: SEPARATOR}))
+    .on('data', (data: CsvModel) => {
+        (validate(data) ? results : errors).push(data);
+    })
+    .on('end', () => {
+        if (NEED_SAVE_DB) {
+            saveToDB(results);
+        }
+        makeCsvFile(errors);
+        console.log('res', results);
+    });
--- csv-to-db/test/test.ts
+++ csv-to-db/test/test.ts
@@ -1,216 +1,262 @@
 import {CsvModel, Email, Phone, RegistrationDate, RegistrationTime} from '../model';
 import * as assert from 'assert';
-import {validate} from '../main';
+import {saveToDB, validate} from '../main';
 import {checkLength, checkMinMax} from '../config';
+import {createConnection} from 'mysql';
+
 
 describe('APP', () => {
-	describe('Models', () => {
-		describe('Email', () => {
-			it('should be ok if email is empty string', () => {
-				new Email('')
-			});
+    describe('Models', () => {
+        describe('Email', () => {
+            it('should be ok if email is empty string', () => {
+                new Email('')
+            });
 
-			it('should be ok if obj is null', () => {
-				new Email(null);
-			});
+            it('should be ok if obj is null', () => {
+                new Email(null);
+            });
 
-			it('should be ok if obj is undefined', () => {
-				new Email(undefined);
-			});
+            it('should be ok if obj is undefined', () => {
+                new Email(undefined);
+            });
 
-			it('should be created if obj is @', () => {
-				new Email('@');
-			});
+            it('should be created if obj is @', () => {
+                new Email('@');
+            });
 
-			it('should be created if obj is 1', () => {
-				const object = new Email('1');
-			});
+            it('should be created if obj is 1', () => {
+                const object = new Email('1');
+            });
 
-			describe('length', () => {
-				it('should return 8 when email is \'v@ni.com\'', () => {
-					const object = new Email('v@ni.com');
+            describe('length', () => {
+                it('should return 8 when email is \'v@ni.com\'', () => {
+                    const object = new Email('v@ni.com');
 
-					assert.strictEqual(object.length, 8);
-				});
+                    assert.strictEqual(object.length, 8);
+                });
 
-				it('should return 0 when email is empty string', () => {
-					const object = new Email('');
+                it('should return 0 when email is empty string', () => {
+                    const object = new Email('');
 
-					assert.strictEqual(object.length, 0);
-				});
-			});
+                    assert.strictEqual(object.length, 0);
+                });
+            });
 
-		});
+        });
 
-		describe('Phone', () => {
-			it('should be ok if email is empty string', () => {
-				new Phone('')
-			});
+        describe('Phone', () => {
+            it('should be ok if email is empty string', () => {
+                new Phone('')
+            });
 
-			it('should be ok if obj is null', () => {
-				new Phone(null);
-			});
+            it('should be ok if obj is null', () => {
+                new Phone(null);
+            });
 
-			it('should be ok if obj is undefined', () => {
-				new Phone(undefined);
-			});
+            it('should be ok if obj is undefined', () => {
+                new Phone(undefined);
+            });
 
-			it('should be created if obj is @', () => {
-				new Phone('@');
-			});
+            it('should be created if obj is @', () => {
+                new Phone('@');
+            });
 
-			it('should be created if obj is 1', () => {
-				const object = new Phone('1');
-			});
+            it('should be created if obj is 1', () => {
+                const object = new Phone('1');
+            });
 
-			describe('length', () => {
-				it('should return 8 when Phone is \'+375-29-1111111\'', () => {
-					const object = new Phone('+375-29-1111111');
+            describe('length', () => {
+                it('should return 8 when Phone is \'+375-29-1111111\'', () => {
+                    const object = new Phone('+375-29-1111111');
 
-					assert.strictEqual(object.length, 13);
-				});
+                    assert.strictEqual(object.length, 13);
+                });
 
-				it('should return 0 when Phone is empty string', () => {
-					const object = new Phone('');
+                it('should return 0 when Phone is empty string', () => {
+                    const object = new Phone('');
 
-					assert.strictEqual(object.length, 0);
-				});
-			});
+                    assert.strictEqual(object.length, 0);
+                });
+            });
 
-		});
+        });
 
-		describe('RegistrationDate', () => {
-			it('should be ok if email is empty string', () => {
-				new RegistrationDate('')
-			});
+        describe('RegistrationDate', () => {
+            it('should be ok if email is empty string', () => {
+                new RegistrationDate('')
+            });
 
-			it('should be ok if obj is null', () => {
-				new RegistrationDate(null);
-			});
+            it('should be ok if obj is null', () => {
+                new RegistrationDate(null);
+            });
 
-			it('should be ok if obj is undefined', () => {
-				new RegistrationDate(undefined);
-			});
+            it('should be ok if obj is undefined', () => {
+                new RegistrationDate(undefined);
+            });
 
-			it('should be created if obj is @', () => {
-				new RegistrationDate('@');
-			});
+            it('should be created if obj is @', () => {
+                new RegistrationDate('@');
+            });
 
-			it('should be created if obj is 1', () => {
-				const object = new RegistrationDate('1');
-			});
+            it('should be created if obj is 1', () => {
+                const object = new RegistrationDate('1');
+            });
 
-			describe('length', () => {
-				it('should return 8 when RegistrationDate is \'12-12-2017\'', () => {
-					const object = new RegistrationDate('12-12-2017');
+            describe('length', () => {
+                it('should return 8 when RegistrationDate is \'12-12-2017\'', () => {
+                    const object = new RegistrationDate('12-12-2017');
 
-					assert.strictEqual(object.length, 8);
-				});
+                    assert.strictEqual(object.length, 8);
+                });
 
-				it('should return 0 when RegistrationDate is empty string', () => {
-					const object = new RegistrationDate('');
+                it('should return 0 when RegistrationDate is empty string', () => {
+                    const object = new RegistrationDate('');
 
-					assert.strictEqual(object.length, 0);
-				});
-			});
+                    assert.strictEqual(object.length, 0);
+                });
+            });
 
-		});
+        });
 
-		describe('RegistrationTime', () => {
-			it('should be ok if email is empty string', () => {
-				new RegistrationTime('')
-			});
+        describe('RegistrationTime', () => {
+            it('should be ok if email is empty string', () => {
+                new RegistrationTime('')
+            });
 
-			it('should be ok if obj is null', () => {
-				new RegistrationTime(null);
-			});
+            it('should be ok if obj is null', () => {
+                new RegistrationTime(null);
+            });
 
-			it('should be ok if obj is undefined', () => {
-				new RegistrationTime(undefined);
-			});
+            it('should be ok if obj is undefined', () => {
+                new RegistrationTime(undefined);
+            });
 
-			it('should be created if obj is @', () => {
-				new RegistrationTime('@');
-			});
+            it('should be created if obj is @', () => {
+                new RegistrationTime('@');
+            });
 
-			it('should be created if obj is 1', () => {
-				const object = new RegistrationTime('1');
-			});
+            it('should be created if obj is 1', () => {
+                const object = new RegistrationTime('1');
+            });
 
-			describe('length', () => {
-				it('should return 8 when RegistrationTime is \'12/48\'', () => {
-					const object = new RegistrationTime('12/48');
+            describe('length', () => {
+                it('should return 8 when RegistrationTime is \'12/48\'', () => {
+                    const object = new RegistrationTime('12/48');
 
-					assert.strictEqual(object.length, 4);
-				});
+                    assert.strictEqual(object.length, 4);
+                });
 
-				it('should return 0 when RegistrationTime is empty string', () => {
-					const object = new RegistrationTime('');
+                it('should return 0 when RegistrationTime is empty string', () => {
+                    const object = new RegistrationTime('');
 
-					assert.strictEqual(object.length, 0);
-				});
-			});
+                    assert.strictEqual(object.length, 0);
+                });
+            });
 
-		});
-	});
+        });
+    });
 
-	describe('Main', () => {
-		describe('Validate function', () => {
-			it('should return false is data is null', () => {
-				assert.strictEqual(validate(null), false);
-			});
+    describe('Main', () => {
+        describe('Validate function', () => {
+            it('should return false is data is null', () => {
+                assert.strictEqual(validate(null), false);
+            });
 
-			it('should return true if data is valid', () => {
-				const data: CsvModel = {
-					id: '1',
-					name: 'Veronika',
-					date: '12-11-2017',
-					time: '12-14',
-					age: '19',
-					email: 'v@mail.ru',
-					phone: '+375-29-1111111'
-				};
+            it('should return true if data is valid', () => {
+                const data: CsvModel = {
+                    id: '1',
+                    name: 'Veronika',
+                    date: '12-11-2017',
+                    time: '12-14',
+                    age: '19',
+                    email: 'v@mail.ru',
+                    phone: '+375-29-1111111'
+                };
 
-				assert.strictEqual(validate(data), true);
-			});
-		});
-	});
+                assert.strictEqual(validate(data), true);
+            });
+        });
 
-	describe('Config', () => {
-		describe('checkLength', () => {
-			it('should return true if value is \'abc\' and min and max = 2 3', () => {
-				assert.strictEqual(checkLength(2,3)('abc'), true);
-			});
+        describe('saveToDB', () => {
+            it('should return if value is []', () => {
+                saveToDB([]);
+            });
 
-			it('should return true if value is \'abc\' and min and max = 3 3', () => {
-				assert.strictEqual(checkLength(3,3)('abc'), true);
-			});
+            it('should create value in db if value is ok', (done) => {
+                const data: CsvModel = {
+                    id: '1',
+                    name: 'Veronika',
+                    date: '12-11-2017',
+                    time: '12-14',
+                    age: '19',
+                    email: 'v@mail.ru',
+                    phone: '+375-29-1111111'
+                };
 
-			it('should return false if value is \'abc\' and min and max = 2 2', () => {
-				assert.strictEqual(checkLength(2,2)('abc'), false);
-			});
-		});
+                saveToDB([data]);
 
-		describe('checkMinMax', () => {
-			it('should return true if value is 5 and min and max = 1 6', () => {
-				assert.strictEqual(checkMinMax(1,6)(5), true);
-			});
+                // we use setTimeout method because of the time we need to
+                // wait until data will be saved to db
+                setTimeout(() => {
+                    const connection = createConnection({
+                        host: 'localhost',
+                        user: 'root',
+                        password: '',
+                        database: 'course_db'
+                    });
 
-			it('should return true if value is 5 and min and max = 1 5', () => {
-				assert.strictEqual(checkMinMax(1,5)(5), true);
-			});
+                    connection.connect((err) => {
+                        if (err) throw err;
+                        const sql = `SELECT * FROM users WHERE id=${data.id}`;
+                        connection.query(sql, (err, result: Array<any>) => {
+                            if (err) throw err;
 
-			it('should return true if value is 5 and min and max = 5 5', () => {
-				assert.strictEqual(checkMinMax(5,5)(5), true);
-			});
+                            assert.strictEqual(result.length, 1);
+                            assert.strictEqual(result[0].id.toString(), data.id.toString());
+                            done();
+                        });
+                    })
 
-			it('should return true if value is 5 and min and max = 5 6', () => {
-				assert.strictEqual(checkMinMax(5,6)(5), true);
-			});
+                }, 1500)
+            });
+        })
+    });
 
-			it('should return true if value is 5 and min and max = 2 3', () => {
-				assert.strictEqual(checkLength(2,3)(5), false);
-			});
-		});
-	});
+    describe('Config', () => {
+        describe('checkLength', () => {
+            it('should return true if value is \'abc\' and min and max = 2 3', () => {
+                assert.strictEqual(checkLength(2, 3)('abc'), true);
+            });
+
+            it('should return true if value is \'abc\' and min and max = 3 3', () => {
+                assert.strictEqual(checkLength(3, 3)('abc'), true);
+            });
+
+            it('should return false if value is \'abc\' and min and max = 2 2', () => {
+                assert.strictEqual(checkLength(2, 2)('abc'), false);
+            });
+        });
+
+        describe('checkMinMax', () => {
+            it('should return true if value is 5 and min and max = 1 6', () => {
+                assert.strictEqual(checkMinMax(1, 6)(5), true);
+            });
+
+            it('should return true if value is 5 and min and max = 1 5', () => {
+                assert.strictEqual(checkMinMax(1, 5)(5), true);
+            });
+
+            it('should return true if value is 5 and min and max = 5 5', () => {
+                assert.strictEqual(checkMinMax(5, 5)(5), true);
+            });
+
+            it('should return true if value is 5 and min and max = 5 6', () => {
+                assert.strictEqual(checkMinMax(5, 6)(5), true);
+            });
+
+            it('should return true if value is 5 and min and max = 2 3', () => {
+                assert.strictEqual(checkLength(2, 3)(5), false);
+            });
+        });
+    });
 });
